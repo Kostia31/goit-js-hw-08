2321b88038c4b4486b3095d2c92e0a164dec8c9a
Kostia31 <kostyapodnebesnuy@gmail.com>
2023-05-04 15:44:06 +0300

    add feedback form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+formRef.addEventListener('input', throttle(getValueOnForm, 500));
+formRef.addEventListener('submit', submitForm);
+
+const dataForm = {};
+
+function getValueOnForm(evt) {
+  dataForm[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+}
+
+function auditLocalStorage() {
+  const feedBack = localStorage.getItem('feedback-form-state');
+  const parsFeedBack = JSON.parse(feedBack);
+  if (feedBack) {
+    formRef.message.value = parsFeedBack.message || '';
+    formRef.email.value = parsFeedBack.email || '';
+  }
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  const emailValue = e.currentTarget.email.value;
+  const messageValue = e.currentTarget.message.value;
+  const relustForm = {}
+  relustForm.email = emailValue;
+  relustForm.message = messageValue;
+  console.log(relustForm);
+  e.currentTarget.reset();
+}
+
+auditLocalStorage();
